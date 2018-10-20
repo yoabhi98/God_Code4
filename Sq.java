db1b0aa9d60be0dd791e5464818590cf7eed7306
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-20 10:54:11 +0530

    Add files via upload

---
+++ Sq.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sq
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+StringBuffer rev=new StringBuffer(str);
+rev.append(".");
+System.out.println(rev);
+}
+}
