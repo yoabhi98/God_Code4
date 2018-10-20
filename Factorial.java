8327de464ed0f3ddca9607222ce3b80b25f31908
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-20 10:31:16 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Abhi10
+{
+
+     
+public static void main(String []args)
+{
+ 
+int fact=5;            
+Scanner sc=new Scanner(System.in);
+
+int num = sc.nextInt();    
+while(fact>0)
+{
+if(num%5==0)
+{
+System.out.print(num+" ");
+num++;
+}
+fact--;
+}
+}
+}
